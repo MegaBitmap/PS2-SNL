6ae91b8514e2a95c95b59ba806ef458e0e7a51b9
MegaBitmap <93689649+MegaBitmap@users.noreply.github.com>
2025-09-19 08:26:53 -0700

    Update to neutrino v1.7.0-30-g607309e-singleFolder

--- SNLManagerSource/SNL-CLI/BSDConf.cs
+++ SNLManagerSource/SNL-CLI/BSDConf.cs
@@ -15,25 +15,7 @@ namespace SNL_CLI
                 "[[module]]\n" +
                 "file = \"smap_udpbd.irx\"\n" +
                 $"args = [\"ip={ps2ip}\"]\n" +
-                "env = [\"LE\", \"EE\"]\n\n" +
-                "# Faking strategy\n" +
-                "# ---------------\n" +
-                "# To prevent games from trying to use networing:\n" +
-                "# - we try to simulate that there is no dev9 hardware present:\n" +
-                "#   - dev9 returns NO_RESIDENT_END, module is hidden\n" +
-                "#   - all modules depending on dev9 fail to load becouse dev9 is not resident\n" +
-                "[[fake]]\n" +
-                "file = \"ENT_SMAP.IRX\"\n" +
-                "name = \"ent_smap\"\n" +
-                "version = 0x021f\n" +
-                "loadrv = -200 # KE_LINKERR becouse dev9 does not exist\n" +
-                "startrv = 1    # 0=RESIDENT_END, 1=NO_RESIDENT_END, 2=REMOVABLE_END\n" +
-                "[[fake]]\n" +
-                "file = \"SMAP.IRX\"\n" +
-                "name = \"INET_SMAP_driver\"\n" +
-                "version = 0x0219\n" +
-                "loadrv = -200 # KE_LINKERR becouse dev9 does not exist\n" +
-                "startrv = 1    # 0=RESIDENT_END, 1=NO_RESIDENT_END, 2=REMOVABLE_END\n";
+                "env = [\"LE\", \"EE\"]\n";
         }
     }
 }
